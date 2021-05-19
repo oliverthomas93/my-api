b20773a557df3218fe1c8f93394647841c65cb0c
Oliver Thomas (IT Services) <o.thomas.1@bham.ac.uk>
2021-05-19 10:15:59 +0100

    Add new routes, controllers and models folders

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import express from 'express';
+import routes from './src/routes/crmRoutes';
 
 let app = express();
 let PORT = 4000;
 
+routes(app);
+
 app.get('/', (req,res) => {
     res.send(`Node and Express server running on port ${PORT}`)
-});
-
-app.listen(PORT, () => {
-    console.log(`Your server is running on port ${PORT}`)
 });
 
 app.listen(PORT, () => {
