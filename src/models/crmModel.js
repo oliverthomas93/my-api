484cac756a48b0b167d71fb4f91193b5322d8869
Oliver Thomas (IT Services) <o.thomas.1@bham.ac.uk>
2021-05-21 08:07:33 +0100

    latest commit

---
+++ src/models/crmModel.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+export const ContactSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String
+    },
+    company: {
+        type: String
+    },
+    phone: {
+        type: Number
+    },
+    created_date: {
+        type: Date,
+        default: Date.now()
+    }
+});
